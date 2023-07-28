ba8ed224d3fe9087678c9d4bf13d4278ef50e2a9
chungvh <chungvh@runsystem.net>
2023-07-28 09:49:16 +0700

    first comnmit

---
+++ pokemon/assets/index-78ab4da4.js
@@ -0,0 +1,1 @@
+import{d as e,c as _,o as n,_ as t}from"./index-45caa8ec.js";const a=e({__name:"index",setup(o){return(s,c)=>(n(),_("main"))}});const d=t(a,[["__scopeId","data-v-684645a0"]]);export{d as default};
